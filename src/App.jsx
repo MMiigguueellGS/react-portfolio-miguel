a4bbed75bc1277c2029c405c75b266723d34ec60
Miguel Guevara <guesian.05.88@gmail.com>
2023-12-12 21:43:00 -0500

    actualizando cv v2

--- src/App.jsx
+++ src/App.jsx
@@ -53,7 +53,7 @@ function App() {
 
           {/* <!-- boton del cv --> */}
           <div className="user-info__buttons">
-            <a href="https://drive.google.com/file/d/1oIYqRqGy1m8lrys2lzMpcZ0tJiqLTKqI/view?usp=drive_link" className="user-info__btn" target="_blank">
+            <a href="https://drive.google.com/file/d/1P5bvugt0Kkkkyfnp3EFonDwtUw1a04IA/view?usp=drive_link" className="user-info__btn" target="_blank">
               Descargar CV
             </a>
           </div>
